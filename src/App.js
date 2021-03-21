632350bb668305788a562d0941c9aed11a01010e
pwkrz <pg.krzeminski@gmail.com>
2021-03-21 17:24:14 +0100

    project start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container vh-100">
+      <header>DOG TRANSLATOR</header>
+      <main>
+        <div className="screen">SCREEN</div>
+        <div className="controls">CONTROLS</div>
+        <div className="speaker">SPEAKER</div>
+      </main>
     </div>
   );
 }
