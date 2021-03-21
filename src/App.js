a6528c772cb37ec1eb27d19b734eab59566b087e
pwkrz <pg.krzeminski@gmail.com>
2021-03-21 18:01:22 +0100

    installed bootstrap in project

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container vh-100">
+    <div className="container vh-100 d-flex flex-column">
       <header>DOG TRANSLATOR</header>
-      <main>
+      <main class="flex-grow-1">
         <div className="screen">SCREEN</div>
         <div className="controls">CONTROLS</div>
         <div className="speaker">SPEAKER</div>
